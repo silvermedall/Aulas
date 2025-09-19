b2d6418f52c02ae02514af2e8fc55270e52e526e
silvermedall <toxicsilver9@gmail.com>
2025-09-19 21:09:04 +0100

    19/9

---
+++ AP/aula_19_09/aula01.py
@@ -0,0 +1,53 @@
+# Exercicio 1
+print("Utilize o interpretador para efectuar os seguintes cálculos:")
+print(45 + 27)
+print(2**3)
+print(9 % 4)
+print("2" + "34")
+print(126.4 * 4)
+print(2 / 5)
+print(2 // 5)
+print("\n")
+# Exercicio 2
+print("Imprima a frase 'Primeira Frase em Python' no ecrã.")
+print("Primeira Frase em Python")
+
+print("Repita a instrução anterior seguida de duas quebras de linha.")
+print("\t\tPrimeira Frase em Python")
+
+print("Obtenha o seguinte resultado com uma única instrução:")
+print("Python Menu\n\t1. Primeira linha\n\t2. Segunda\n\t3. Última linha")
+
+print("Leia a frase 'Python' da shell.")
+print(input())
+
+print(
+    "Leia a frase 'Python' na shell e concatene-a com a frase '3.5' numa única instrução."
+)
+
+print("3.5 " + input())
+
+print("Leia e concatene duas frases da shell numa única instrução.")
+print(input() + input())
+
+print("Leia um número da shell.")
+print(int(input()))
+
+print("Leia um número da shell e some-o com 2.")
+print(int(input()) + 2)
+
+print("Leia um número da shell, some-o com 2 e multiplique o resultado por 2.")
+print((int(input()) + 2) * 2)
+
+print("Leia e some dois números da shell.")
+print(int(input()) + int(input()))
+
+print(
+    "Leia um número da shell, some-o por 2 e multiplique o resultado por outro número lido da shell."
+)
+print((int(input()) + 2) * int(input()))
+
+print(
+    "Leia dois números da shell e obtenha o resto da divisão do primeiro pelo segundo."
+)
+print(int(input()) % int(input()))
